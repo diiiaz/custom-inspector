1b9fbec1a15ac053856fa3bdec8815544d27b7b0
Diaz <cantonival@gmail.com>
2025-05-29 22:24:45 +0200

    Added MenuButton as a basic control builder

---
+++ addons/custom_inspector/control_builders/basic/menu_button.gd
@@ -0,0 +1,72 @@
+extends CIButton
+class_name CIMenuButton
+
+
+static func enum_to_items(enumerator: Dictionary) -> Array[CIMenuItem]:
+	var items: Array[CIMenuItem] = []
+	for index: int in range(enumerator.size()):
+		var key: String = enumerator.keys()[index]
+		items.append(CIMenuItem.new().set_text(key))
+	return items
+
+
+func setup_items(items: Array[CIMenuItem]) -> CIMenuButton:
+	add_build_setter(
+		func(menu_button: MenuButton):
+			for index: int in range(items.size()):
+				var item: CIMenuItem = items[index]
+				menu_button.get_popup().add_item(item.get_text().capitalize(), index)
+				menu_button.get_popup().set_item_icon(index, item.get_icon())
+				menu_button.get_popup().set_item_as_checkable(index, item.is_checkable())
+				menu_button.get_popup().set_item_disabled(index, item.is_disabled())
+				menu_button.get_popup().set_item_as_separator(index, item.is_separator())
+	)
+	return self
+
+
+func set_item_selected_callable(callable: Callable) -> CIBaseButton:
+	add_build_setter(func(menu_button: MenuButton): menu_button.get_popup().id_pressed.connect(callable))
+	return self
+
+
+func build(parent: Control = null) -> Control:
+	var menu_button: MenuButton = MenuButton.new()
+	menu_button.flat = false
+	menu_button.theme_type_variation = "InspectorActionButton"
+	finish_control_setup(menu_button, parent)
+	return menu_button
+
+
+class CIMenuItem extends RefCounted:
+	var _text: String = ""
+	var _icon: Texture2D = null
+	var _checkable: bool = false
+	var _disabled: bool = false
+	var _separator: bool = false
+	
+	func get_text() -> String: return _text
+	func get_icon() -> Texture2D: return _icon
+	func is_checkable() -> bool: return _checkable
+	func is_disabled() -> bool: return _disabled
+	func is_separator() -> bool: return _separator
+	
+	func set_text(text: String) -> CIMenuItem:
+		_text = text
+		return self
+	
+	func set_icon(icon: Texture2D) -> CIMenuItem:
+		_icon = icon
+		return self
+	
+	func set_checkable(checkable: bool) -> CIMenuItem:
+		_checkable = checkable
+		return self
+	
+	func set_disabled(disabled: bool) -> CIMenuItem:
+		_disabled = disabled
+		return self
+	
+	func set_separator(separator: bool) -> CIMenuItem:
+		_separator = separator
+		return self
+	
