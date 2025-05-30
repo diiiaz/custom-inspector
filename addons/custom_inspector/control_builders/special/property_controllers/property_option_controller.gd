f315192404487779c853bd235e2c996765c618f0
Diaz <cantonival@gmail.com>
2025-05-30 16:37:36 +0200

    Fixed CIOptionButton trying to select value when there is options

--- addons/custom_inspector/control_builders/special/property_controllers/property_option_controller.gd
+++ addons/custom_inspector/control_builders/special/property_controllers/property_option_controller.gd
@@ -9,10 +9,10 @@ func load_enum(enumerator: Dictionary) -> CIPropertyOptionController:
 
 func build(parent: Control = null) -> Control:
 	var option_button: OptionButton = CIOptionButton.new().build()
-	option_button.select(_value)
 	option_button.item_selected.connect(
 		func(item_index: int):
 			set_value(item_index)
 	)
 	finish_control_setup(option_button, parent)
+	option_button.select(_value)
 	return option_button
