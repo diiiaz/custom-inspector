e9291538f0417957df719502be1b8a4f620d26d8
Diaz <cantonival@gmail.com>
2025-05-30 17:38:39 +0200

    Option controller's enum loader works with non-linear index mapping
    i don't really know how to describe it

--- addons/custom_inspector/control_builders/special/property_controllers/property_option_controller.gd
+++ addons/custom_inspector/control_builders/special/property_controllers/property_option_controller.gd
@@ -1,10 +1,12 @@
 extends CIPropertyController
 class_name CIPropertyOptionController
 
-var _items: Dictionary
+var _remap: Dictionary
 
 func load_enum(enumerator: Dictionary) -> CIPropertyOptionController:
 	add_build_setter(CIHelper.load_enum_items_in_option_button.bind(enumerator))
+	for index: int in range(enumerator.size()):
+		_remap[enumerator.values()[index]] = index
 	return self
 
 func load_string_array(array: Array[String]) -> CIPropertyOptionController:
@@ -15,8 +17,8 @@ func build(parent: Control = null) -> Control:
 	var option_button: OptionButton = CIOptionButton.new().build()
 	option_button.item_selected.connect(
 		func(item_index: int):
-			set_value(item_index)
+			set_value(int(_remap.keys()[item_index]))
 	)
 	finish_control_setup(option_button, parent)
-	option_button.select(_value)
+	option_button.select(_remap[_value])
 	return option_button
