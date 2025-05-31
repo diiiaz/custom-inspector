067ac6fbd2ceff1044e76a527e45849e7038b6ab
Diaz <cantonival@gmail.com>
2025-05-31 09:59:18 +0200

    Added CIGridContainer

---
+++ addons/custom_inspector/control_builders/basic/grid_container.gd
@@ -0,0 +1,12 @@
+extends CIBaseButton
+class_name CIGridContainer
+
+
+func _init(columns: int) -> void:
+	add_build_setter(func(grid_container: GridContainer): grid_container.columns = columns)
+
+
+func build(parent: Control = null) -> Control:
+	var grid_container: GridContainer = GridContainer.new()
+	finish_control_setup(grid_container, parent)
+	return grid_container
