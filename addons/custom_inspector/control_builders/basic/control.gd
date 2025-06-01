6c56a1a2a7269f2110d60b8bd53c1d1fa2f10283
Diaz <cantonival@gmail.com>
2025-06-01 22:35:10 +0200

    Added CIControl

---
+++ addons/custom_inspector/control_builders/basic/control.gd
@@ -0,0 +1,8 @@
+extends CIBase
+class_name CIControl
+
+
+func build(parent: Control = null) -> Control:
+	var control: Control = Control.new()
+	finish_control_setup(control, parent)
+	return control
