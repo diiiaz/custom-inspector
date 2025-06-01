f609f87c3f3e801af6487f0bd1cf39e9ee6e2299
Diaz <cantonival@gmail.com>
2025-06-01 22:34:56 +0200

    Added set_flat in CIButton

--- addons/custom_inspector/control_builders/basic/button.gd
+++ addons/custom_inspector/control_builders/basic/button.gd
@@ -10,6 +10,10 @@ func set_icon(icon: String) -> CIButton:
 	add_build_setter(func(button: Button): button.set_button_icon(CIHelper.get_icon(icon)))
 	return self
 
+func set_flat() -> CIButton:
+	add_build_setter(func(button: Button): button.flat = true)
+	return self
+
 func build(parent: Control = null) -> Control:
 	var button: Button = Button.new()
 	finish_control_setup(button, parent)
